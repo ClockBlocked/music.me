c603553c73d2c034e25a65b864787d573776a609
Cole Hanson <antagoniise@gmail.com>
2026-09-25 02:07:51 -0500

    Update library URL to new GitHub source

--- source/boot.js
+++ source/boot.js
@@ -58,16 +58,16 @@ jQuery(function ($) {
   function loadLibrary() {
     let jsonUrl;
     try {
-      jsonUrl = new URL("https://mybeats.cloud/source/library.json", window.__MYBEATS_BASE__).href;
+      jsonUrl = new URL("https://clockblocked.github.io/music.me/source/library.json", window.__MYBEATS_BASE__).href;
     } catch {
-      jsonUrl = "https://mybeats.cloud/source/library.json";
+      jsonUrl = "https://clockblocked.github.io/music.me/source/library.json";
     }
 
     const candidates = [
       jsonUrl,
-      "https://mybeats.cloud/source/library.json",
-      "/library.json",
-      "./library.json",
+      "https://clockblocked.github.io/music.me/source/library.json",
+//      "/library.json",
+//      "./library.json",
     ].filter((v, i, a) => v && a.indexOf(v) === i);
 
     return tryNext(0);
